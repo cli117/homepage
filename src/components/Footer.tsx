26fb3b25926bb2b11770e9c5d38832720a094bce
cli117 <cheng.li2@mail.mcgill.ca>
2025-03-04 23:28:39 +0800

    Content fill

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
         <a href="https://github.com/cli117" target="_blank" rel="noreferrer"><GitHubIcon/></a>
         <a href="https://www.linkedin.com/in/cheng-li-8272a3121/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/cli117/homepage" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>A portfolio designed & built by <a href="https://github.com/cli117/homepage" target="_blank" rel="noreferrer">Cheng Li</a> with 💜</p>
     </footer>
   );
 }
